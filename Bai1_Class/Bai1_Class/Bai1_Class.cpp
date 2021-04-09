709a0d11c365410d180ec1b8b3ab50df3ce40b3d
La Quốc Thắng <quocthang0507@gmail.com>
2021-04-09 16:05:10 +0700

    Update Bài1

--- Bai1_Class/Bai1_Class/Bai1_Class.cpp
+++ Bai1_Class/Bai1_Class/Bai1_Class.cpp
@@ -1,5 +1,14 @@
 // Bai1_Class.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Lớp là sự biểu diễn của đối tượng trong lập trình và ngược lại, đối tượng là sự thể hiện của lớp.
+// Một đối tượng gồm có: thuộc tính và phương thức. Chúng ta có thể xem một lớp là kiểu dữ liệu còn đối tượng là biến.
+
+/*
+	class tên_lớp {
+		các_thuộc_tính;
+		các_phương_thức;
+	} [tên_đối_tượng];
+	Nhìn giống khai báo struct nhỉ!
+*/
 
 #include <iostream>
 
@@ -7,15 +16,19 @@ using namespace std;
 
 class Human
 {
+	// Mặc định là private
 	string Name;
 	int Age;
 public:
+	// Khai báo nguyên mẫu hàm
+	// Các hàm nhập giá trị setter và nhận giá trị getter
 	void SetName(string);
 	void SetAge(int);
 	string GetName(void);
 	int GetAge(void);
 };
 
+// Để xác định phương thức thuộc một lớp nào đó, ta sử dụng toán tử phạm vi :: theo sau tên lớp
 void Human::SetName(string name)
 {
 	this->Name = name;
@@ -45,13 +58,10 @@ int main()
 	return 0; // run ok
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+/*
+	* Phạm vi truy cập của phương thức là: private, protected, public
+	** private: các thành viên của cùng một lớp hoặc từ hàm bạn của nó có thể truy cậP.
+	** protected: các thành viên của cùng một lớp hoặc từ hàm bạn của nó hoặc từ một lớp dẫn xuất của nó hoặc 
+	bạn của một lớp dẫn xuất của nó đều có thể truy cập.
+	** public: các thành viên có thể truy cập lẫn nhau từ mọi lớp.
+*/
